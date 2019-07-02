0d7b8ee93a08620e098cc81360feebf0de6829c5
derek_ramm <derek_ramm@icloud.com>
2019-07-02 18:52:32 +0800

    简单工厂代码

--- pattern_behavioral/memento/gof_memento.py
+++ pattern_behavioral/memento/gof_memento.py
@@ -1,15 +1,48 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-"""gof_memento.py"""
+"""gof_memento.py
+备忘录模式
+"""
+
 
 class Product(object):
-    def __init__(self, name, price): self.name, self.price = name, price
-    def save(self): return Memento(self.name, self.price)
-    def load(self, m): self.name, self.price = m.name, m.price
+    """产品类"""
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def save(self):
+        """
+        将产品内部的状态存储到备忘录对象
+        :return: 备忘录对象
+        """
+        return Memento(self.name, self.price)
+
+    def load(self, m):
+        """
+        将备忘录中存储的状态恢复到产品对象中
+        :param m: 备忘录对象
+        :return:
+        """
+        self.name, self.price = m.name, m.price
+
+
 class Memento(object):
-    def __init__(self, name, price): self.name, self.price = name, price
+    """备忘录"""
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
 class Director(object):
-    def __init__(self): self.mem = None
+    """导演类"""
+
+    def __init__(self):
+        """关联备忘录"""
+        self.mem = None
+
 
 if __name__ == '__main__':
     d, p = Director(), Product('A', 10)
@@ -17,6 +50,3 @@ if __name__ == '__main__':
     p.name, p.price = 'B', 20
     p.load(d.mem)
     print(p.__dict__)
-
-
-
